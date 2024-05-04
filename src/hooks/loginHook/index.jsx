071aa725799c739ab0f20e5ab922e310de2c12d5
ThomasGrare <thomasgrare64@gmail.com>
2024-05-05 00:50:54 +0200

    fix axios request login

--- src/hooks/loginHook/index.jsx
+++ src/hooks/loginHook/index.jsx
@@ -20,7 +20,7 @@ export default function LoginHook() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:8080/login', { email, password });
+      const res = await axios.post('http://localhost:8080/login', { ...values });
       const token = res.headers.authorization.split(' ')[1];
       const userId = res.data.user._id;
       localStorage.setItem('userId', userId);
