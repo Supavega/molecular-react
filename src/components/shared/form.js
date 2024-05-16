ff65b4486c939faa88111556c676a6ba5414cde3
saithomas-gbz <thomasgrare64@gmail.com>
2024-05-16 11:00:04 +0200

    quick redesign of auth form

--- src/components/shared/form.js
+++ src/components/shared/form.js
@@ -1,10 +1,26 @@
 import { styled } from "styled-components";
+import { Button } from "primereact/button";
+
+
 
 export const CenterForm = styled.form`
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 10px;
+  padding: 50px;
+  border-radius: 50px;
+  background: #e0e0e0;
+  box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
 `;
 
+export const FormButton = styled(Button)`
+  width: 100%;
+  
+`;
+
+export const SubmitContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
