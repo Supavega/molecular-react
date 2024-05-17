7fc8a2defaec79ecb20b011b161f69310be02d2d
Rémy <remy.berjonneau@gmail.com>
2024-05-17 19:02:31 +0200

    file workspace style + creation file style

--- src/components/File/listFile/index.jsx
+++ src/components/File/listFile/index.jsx
@@ -1,5 +1,8 @@
 import useFileList from "../../../hooks/fileHook/listFileHook";
 import { useEffect, useState, useCallback } from "react";
+import { Card } from "primereact/card";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function FileList() {
     const { loadFileList } = useFileList();
@@ -19,16 +22,36 @@ export default function FileList() {
     }, [fetchFileList]);
     
     return (
-        <>
-        {
-            fileList && fileList.map((fileList, index) => (
-            <div key={index}>
-                <h1>
-                <a href={`/edition/${fileList._id}`}>{fileList.name}</a>
-                </h1>
-            </div>
-            ))
-        }
-        </>
+        <CardContainer>
+            {
+                fileList && fileList.map((file, index) => (
+                <StyledCard 
+                    key={index} 
+                    title={<Link to={`/edition/${file._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>{file.name}</Link>}
+                >
+                    <div className="content">
+                        {file.content}
+                    </div>
+                </StyledCard>
+                ))
+            }
+        </CardContainer>
     )
 }
+
+const StyledCard = styled(Card)`
+    margin: 2em;
+    border-radius: 15px;
+    width: calc(33.33% - 4em);
+    .p-card-title, .content{
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+`;
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
